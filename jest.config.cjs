4f8c558195df3844bf72f2eed995115237da7c14
Ersun Warncke <ersun.warncke@gmail.com>
2026-04-27 15:39:20 -0700

    add coverage

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,19 +1,28 @@
 module.exports = {
-  preset: "ts-jest/presets/default-esm",
-  testEnvironment: "node",
+  preset: 'ts-jest/presets/default-esm',
+  testEnvironment: 'node',
   globals: {
     jest: true,
   },
   moduleNameMapper: {
-    "^(\\.{1,2}/.*)\\.js$": "$1",
+    '^(\\.{1,2}/.*)\\.js$': '$1',
   },
   transform: {
-    "^.+\\.tsx?$": [
-      "ts-jest",
+    '^.+\\.tsx?$': [
+      'ts-jest',
       {
         useESM: true,
-        tsconfig: "tsconfig.json",
+        tsconfig: 'tsconfig.json',
       },
     ],
+  },
+  collectCoverageFrom: ['src/**/*.ts', '!src/index.ts'],
+  coverageThreshold: {
+    global: {
+      branches: 50,
+      functions: 50,
+      lines: 50,
+      statements: 50,
+    },
   },
 };
